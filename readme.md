aa5ad722e391a011151cae73efd582d3f357c1a0
bardackx <bardackx@gmail.com>
2020-03-16 14:17:13 -0600

    first commit

---
+++ readme.md
@@ -0,0 +1,20 @@
+# Generador de archivos RPT de COMPAQ
+## Columnas
+En este textarea debe ir un CSV (donde la primer columna es el nombre de la columna del resultado de la consulta SQL y la segunda es el titulo de la columna en el reporte de salida que genera COMPAQ) como el siguiente
+```
+col1, Columna uno
+col2, Columna dos
+dol3, Etcetera
+```
+## SQL
+Aqui va la consulta SQL del reporte
+
+## RPT
+Aqui queda el RPT generado a partir de **Columnas** y **SQL**.
+
+## TODO
+Sería bueno tener las siguientes funcionalidades:
+- Parametros
+- Llenar en automatico column y header a partir del SQL cargado
+- Librería de funciones
+- Usar el atributo format
---
+++ sql-to-rpt.js
@@ -0,0 +1,86 @@
+class TextBuilder {
+
+    eol = "\r\n"
+    text = ""
+
+    ln(text) {
+        this.text += (text ? text : '') + this.eol;
+        return this
+    }
+
+    ap(text) {
+        this.text += text;
+        return this
+    }
+
+    /**
+     * 
+     * @param {any[]} values 
+     * @param {string} comma 
+     */
+    csv(values, comma = ',') {
+        let text = '';
+        for (let i = 0; i < values.length; i++) {
+            text += values[i];
+            if (i < values.length - 1) text += comma
+        }
+        this.ap(text)
+        return this
+    }
+
+    getText() {
+        return this.text
+    }
+}
+
+/**
+ * 
+ * @param {string} sql 
+ * @param {{column:string, header:string, format:string}[]} entries
+ */
+export function generateRpt(sql, entries = []) {
+
+    sql = sql.replace(/'/g, '\'')
+
+    let b = new TextBuilder();
+
+    b.ln("// Parametros de linea de comando")
+    b.ln("gServidor   = @parametroConsola('Servidor')")
+    b.ln("gInstancia  = @parametroConsola('Instancia')")
+    b.ln("gUsuarioSQL = @parametroConsola('NombreUsuario')")
+    b.ln("gClaveSQL   = @parametroConsola('Clave')")
+    b.ln("pQueTipo    = @parametroConsola('QueTipo')")
+    b.ln();
+
+    b.ln("// Conexión")
+    b.ln("Conexion tEmpresa = Conexion.crear('jdbc:jtds:sqlserver:' & gServidor & ';instance=' & gInstancia,gUsuarioSQL,gClaveSQL)")
+    b.ln();
+
+    b.ln("// Parametros")
+    b.ln("Incluye LibParametros.rpt")
+    b.ln("Parametros")
+    b.ln("FinParametros")
+    b.ln()
+
+    let lines = sql.trim().replace(/\r/g, "").split('\n');
+    b.ln("// SQL del reporte (" + lines.length + ")")
+    b.ln("sql = ''")
+    for (let line of lines)
+        b.ln("sql = sql & '" + line + "' ")
+    b.ln()
+
+    if (entries && entries.length) {
+        b.ln('// Salida y columnas')
+        b.ln('Consulta tConsulta = tEmpresa[qReporte]')
+        b.ap('Columnas ' + entries.length + ';').csv(entries.map(e => 5)).ln()
+        b.ap('Lista ').csv(entries.map(e => "'" + e.column + "'")).ln()
+        b.ln('Mientras tConsulta->Encontro')
+        b.ap('    ').csv(entries.map(e => e.column).map(e => "tConsulta('" + e + "')")).ln()
+        b.ln('    tConsulta.Busca Siguiente')
+        b.ln('FinMientras')
+        b.ln()
+    }
+
+    return b.getText();
+}
+
